fdc7a75f871819f369d91b4954be04342d86d182
widikumpark <68687823+widikumpark@users.noreply.github.com>
2021-06-24 01:52:29 +0100

    search removed

--- resources/views/vendor/adminlte/partials/navbar/menu-item.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/menu-item.blade.php
@@ -1,11 +1,11 @@
 @inject('menuItemHelper', \JeroenNoten\LaravelAdminLte\Helpers\MenuItemHelper)
 
-@if ($menuItemHelper->isSearchBar($item))
+{{-- @if ($menuItemHelper->isSearchBar($item))
 
     {{-- Search form --}}
-    @include('adminlte::partials.navbar.menu-item-search-form')
+    @include('adminlte::partials.navbar.menu-item-search-form') --}}
 
-@elseif ($menuItemHelper->isSubmenu($item))
+@if ($menuItemHelper->isSubmenu($item))
 
     {{-- Dropdown menu --}}
     @include('adminlte::partials.navbar.menu-item-dropdown-menu')
--- resources/views/vendor/adminlte/partials/sidebar/menu-item.blade.php
+++ resources/views/vendor/adminlte/partials/sidebar/menu-item.blade.php
@@ -7,10 +7,10 @@
         {{ is_string($item) ? $item : $item['header'] }}
     </li>
 
-@elseif ($menuItemHelper->isSearchBar($item))
+{{-- @elseif ($menuItemHelper->isSearchBar($item))
 
     {{-- Search form --}}
-    @include('adminlte::partials.sidebar.menu-item-search-form')
+    {{-- @include('adminlte::partials.sidebar.menu-item-search-form') --}} --}}
 
 @elseif ($menuItemHelper->isSubmenu($item))
 
